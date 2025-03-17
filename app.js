82c9b567efc69fc09e2aabec5bc031dec53a292d
emmanuelasidigbe <emmanuel.asidigbe@amalitech.com>
2025-03-17 08:16:41 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
@@ -16,5 +18,5 @@ app.set("views", path_1.default.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", url_route_1.default);
 app.listen(PORT, () => {
-    console.log(`sever running on port http://localhost:${PORT}`);
+  console.log(`sever listening on port http://localhost:${PORT}`);
 });
